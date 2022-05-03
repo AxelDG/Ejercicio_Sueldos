14e94fbdd8752dc7e6f344c475cd1d0556c207b1
AxelDG <104107385+AxelDG@users.noreply.github.com>
2022-05-02 22:52:14 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+let dato = document.getElementById("dato");
+let btnEnviar = document.getElementById("enviar");
+
+btnEnviar.addEventListener("click", () => {
+  let sueldo: number = Number(dato.value) * 1.2;
+  let sueldo1: number = Number(dato.value) * 1.1;
+  let sueldo2: number = Number(dato.value) * 1.05;
+  let sueldo3: number = Number(dato.value);
+
+  if (sueldo3 > 0 && sueldo3 < 15000) {
+    console.log("Usted tiene un aumento");
+    console.log(sueldo);
+  } else if (sueldo3 > 15001 && sueldo3 < 20000) {
+    console.log("Usted tiene un aumento");
+    console.log(sueldo1);
+  } else if (sueldo3 > 20001 && sueldo3 < 25000) {
+    console.log("Usted tiene un aumento");
+    console.log(sueldo2);
+  } else if (sueldo3 > 25000) {
+    console.log("Usted no tiene un aumento");
+    console.log(sueldo3);
+  }
+});
