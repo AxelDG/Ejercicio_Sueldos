ebcf2b4d377f6627d4835342250d19cd74b06833
AxelDG <104107385+AxelDG@users.noreply.github.com>
2022-05-09 20:29:40 -0300

    Ejercicio_Sueldos

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ btnEnviar.addEventListener("click", () => {
   let sueldo3: number = Number(dato.value);
 
   if (sueldo3 > 0 && sueldo3 < 15000) {
-    console.log("Usted tiene un aumento del 20%");
+    console.log("Usted tiene un aumento del 20% ");
     console.log(sueldo);
   } else if (sueldo3 > 15001 && sueldo3 < 20000) {
-    console.log("Usted tiene un aumento del 10%");
+    console.log("Usted tiene un aumento del 10% ");
     console.log(sueldo1);
   } else if (sueldo3 > 20001 && sueldo3 < 25000) {
-    console.log("Usted tiene un aumento del 5%");
+    console.log("Usted tiene un aumento del 5% ");
     console.log(sueldo2);
   } else if (sueldo3 > 25000) {
     console.log("Usted no tiene un aumento");
